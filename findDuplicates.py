23c7b810f54f1362a8790679f999ef78e7ed85e8
Barend Nieuwoudt <47162589+BarendNieuwoudt@users.noreply.github.com>
2020-10-22 22:38:34 +0200

    Added Comments

--- findDuplicates.py
+++ findDuplicates.py
@@ -1,24 +1,33 @@
+# This python script will search through all the files and print the duplicates
+
 import sys, os
 
 if __name__ == '__main__':
 
 	foldersToSearch = []
+	
+	# Start with searching the current folder
 	foldersToSearch.append('./')
 	
 	foundFiles = {}
 	
 	for currentFolder in foldersToSearch:
 	
+		# Return all the files/folders in the currently searched directory
 		listFilesDirectories = os.listdir(currentFolder)
 	
+		# Once a folder has been searched, it can be removed
 		foldersToSearch.remove(currentFolder)
 		
 		for item in listFilesDirectories:
 			if '.' in item:
+				# Files have extentions
 				if item in foundFiles:
+					# This file name has been found before, print both locations
 					print(currentFolder + item + ' : ' + str(foundFiles.get(item)))
 				else:
 					foundFiles[item] = currentFolder + item + '/'
 			else:
+				# This item is a directory, add it to be searched
 				foldersToSearch.append(currentFolder + item + '/')
 		
