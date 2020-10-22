e0760829db67266d31894c327416de4f3c3d7822
Barend Nieuwoudt <47162589+BarendNieuwoudt@users.noreply.github.com>
2020-10-22 22:48:10 +0200

    Adding Comments

--- organizeFiles.py
+++ organizeFiles.py
@@ -1,13 +1,20 @@
-# python organizeFiles.py ./ *.psd ./STUFFHERE
-# moves files in the current directory, ending on .psd into a STUFFHERE folder
+# python organizeFiles.py <starting directory> <file name regex> <destination directory>
+# for example: python organizeFiles.py ./ *.txt ./SomeTextFiles
+# moves files in the current directory, matching the regex into the specified directory
 
 import glob, sys, os
 
 def move(location, file, newFolder):
+	
 	if len(glob.glob(newFolder)) == 0:
+		# Destination Directory does not exist, create it
 		os.mkdir(newFolder)
+		
+	# Get all the files matching the regex
 	files = glob.glob(location + file)
+	
 	for x in files:
+		# Add files to Destination Directory
 		os.rename(x, newFolder + x)
 		
 if __name__ == "__main__":
